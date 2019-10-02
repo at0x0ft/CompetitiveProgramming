200396170bee9cb5f489bb6b7ba2c7c86dd34a74
at0x0ft <at0x0ft@users.noreply.github.com>
2019-10-03 01:57:21 +0900

    Practice ABC051/A,C and ABC085/A,B,D.

---
+++ atcoder/abc085/d.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Repetitions
+#define FOR(i,a,n) for(ll i = ((ll) a); i < ((ll) n); ++i)
+#define FORR(i, s, g) for(ll i = ((ll)s - 1); i >= ((ll) g); --i)
+#define REP(i, n) FOR(i, 0, n)
+#define REPR(i, n) FORR(i, n, 0)
+
+// Container Utils
+#define ALL(v) (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define SORTALL(v) sort(ALL(v))
+#define SZ(a) int((a).size())
+#define CLR(a) memset((a), 0 ,sizeof(a))
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+
+// For Debug
+#define DMP(x)  cerr << #x << " = " << (x) << endl
+#define DBG(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl
+
+// Aliases
+#define PB push_back
+#define MP make_pair
+typedef long long ll;
+typedef string str;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+// I/O Asynchronizing
+struct aaa{
+    aaa(){
+        cin.tie(0); ios::sync_with_stdio(0); cout<<fixed<<setprecision(20);
+    };
+}aaaaaaa;
+
+int main() {
+    int n, h;
+    cin >> n >> h;
+    vi b(n);
+    int ma = INT_MIN;
+    REP(i, n) {
+        int a;
+        cin >> a >> b[i];
+        ma = max(ma, a);
+    }
+    sort(ALL(b), greater<int>());
+    int cnt = 0;
+    for(cnt = 0; cnt < n && h > 0; cnt++) {
+        if(b[cnt] > ma) h -= b[cnt];
+        else break;
+    }
+    cout << cnt + max(0, ((h + ma - 1) / ma)) << "\n";
+    return 0;
+}
