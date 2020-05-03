e6601fb3fae15614cc430af81b88fea947d15f4c
at0x0ft <at0x0ft@users.noreply.github.com>
2020-05-03 23:32:12 +0900

    Solved ABC165/C.

--- atcoder/ABC/165/c.py
+++ atcoder/ABC/165/c.py
@@ -1,11 +1,24 @@
 n, m, q = map(int, input().split())
 req = []
-[req.append(tuple(map(int, input().split())), True) for _ in range(q)]
-req.sort()
-i = 0
-while True:
-    r, t = req[i]
-    if t:
+[req.append(tuple(map(int, input().split()))) for _ in range(q)]
+a = [0 for _ in range(n)]
 
-    i += 1
-print(req)
+def check():
+    res = 0
+    for i in range(q):
+        ra, rb, rc, rd = req[i]
+        if a[rb - 1] - a[ra - 1] == rc:
+            res += rd
+    return res
+
+def set_a(idx: int, au: int):
+    ans = 0
+    for i in range(au, m + 1):
+        a[idx] = i
+        if idx + 1 < n:
+            ans = max(set_a(idx + 1, i), ans)
+        else:
+            ans = max(check(), ans)
+    return ans
+
+print(set_a(0, 1))
